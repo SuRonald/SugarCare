48de19e1a96769e6e8cae3b63d6d8d725e3c0bdb
Ronald Sumichael <Ronald.Sumichael@gmail.com>
2023-05-25 18:25:53 +0700

    Warning feature

--- SugarCare/View/Components/SugarContainerScene.swift
+++ SugarCare/View/Components/SugarContainerScene.swift
@@ -13,6 +13,8 @@ class SugarContainerScene: SKScene {
     var nodeCount: Int
     var nodeType: String
     
+    let fadeInOut = SKAction.sequence([.fadeIn(withDuration: 0.5), .fadeOut(withDuration: 0.5)])
+    
     init(_ nodeCount: Int, nodeType: String) {
         self.nodeCount = nodeCount
         self.nodeType = nodeType
@@ -29,15 +31,25 @@ class SugarContainerScene: SKScene {
     }
     
     override func didMove(to view: SKView) {
-        print(nodeType)
+//        print(nodeType)
+        let standardSize = UIScreen.main.bounds.size.width / 3
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         
         for _ in 0..<nodeCount {
             var node = SKSpriteNode(imageNamed: ["\(nodeType)/1", "\(nodeType)/2", "\(nodeType)/3"].randomElement()!)
-            if nodeCount > Int(UserHealthViewModel.shared.recomendSugar) && nodeType == "Sugar" {
-                node = SKSpriteNode(imageNamed: ["Sugar/4", "Sugar/5", "Sugar/6"].randomElement()!)
+            
+            if nodeCount > Int(UserHealthViewModel.shared.limitSugarGrams) && nodeType == "Sugar" {
+                node = SKSpriteNode(imageNamed: "Sugar/6")
+                
+                makeWarning("Xwarning")
             }
+            else if nodeCount > Int(UserHealthViewModel.shared.recomendSugar) && nodeType == "Sugar" {
+                node = SKSpriteNode(imageNamed: ["Sugar/4", "Sugar/5"].randomElement()!)
+                
+                makeWarning("Ywarning")
+            }
+            
             node.position = view.center
             node.size = CGSize(width: 60, height: 40)
             node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 60, height: 40))
@@ -55,5 +67,15 @@ class SugarContainerScene: SKScene {
     
     func substractSugar() {
         self.children.first?.removeFromParent()
+    }
+    
+    func makeWarning(_ imageName: String) {
+        let warningNode = SKSpriteNode(imageNamed: imageName)
+        warningNode.position = view.center
+        warningNode.size = CGSize(width: standardSize, height: standardSize)
+        warningNode.alpha = 0
+        self.addChild(warningNode)
+        
+        warningNode.run(SKAction.repeatForever(fadeInOut))
     }
 }
